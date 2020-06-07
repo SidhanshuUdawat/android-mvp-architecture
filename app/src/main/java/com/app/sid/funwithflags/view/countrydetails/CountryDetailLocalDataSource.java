ee5ec98723cb2c3c5066435f7efe15d4f825547c
Sidhanshu Udawat <sidhanshu.udawat@autoscout24.com>
2020-06-07 23:18:20 +0200

    Using realm to read data from database

--- app/src/main/java/com/app/sid/funwithflags/view/countrydetails/CountryDetailLocalDataSource.java
+++ app/src/main/java/com/app/sid/funwithflags/view/countrydetails/CountryDetailLocalDataSource.java
@@ -2,21 +2,30 @@ package com.app.sid.funwithflags.view.countrydetails;
 
 import com.app.sid.funwithflags.datasets.remote.Country;
 import com.app.sid.funwithflags.datasets.remote.SelectedCountry;
+import com.app.sid.funwithflags.model.realm.RealmCountry;
 import com.app.sid.funwithflags.realm.RealmManager;
 
+import io.realm.Realm;
 import rx.Observable;
-
-/**
- * Created by Sidhanshu Udawat on 19-Jan-18.
- */
 
 public class CountryDetailLocalDataSource implements CountryDetailMvp.LocalDataSource {
 
+    private RealmManager mRealManager;
+
     public CountryDetailLocalDataSource(RealmManager realmManager) {
+        mRealManager = realmManager;
     }
 
     @Override
     public Observable<Country> getCountry(SelectedCountry country) {
-        return null;
+        Realm realm = mRealManager.getRealm();
+        try {
+            RealmCountry realmCountry = realm.where(RealmCountry.class)
+                    .contains("name", country.getName())
+                    .findFirst();
+            return Observable.just(realmCountry.asApiModel());
+        } finally {
+            mRealManager.closeRealm(realm);
+        }
     }
 }
